0c87890d68fab8f886465e857841568209d3edf8
Jingjing Shi <jingssrs@users.noreply.github.com>
2024-12-24 16:09:07 +0900

    preprocess the target list ready for netflow run

---
+++ src/utils/common_imports.py
@@ -0,0 +1,32 @@
+'''
+Common imports for the project
+'''
+# basic 
+import numpy as np
+import time
+import os
+
+# io 
+from astropy.io import fits
+from astropy.table import Table
+
+# plot 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import rc
+
+rc('font', **{'family':'sans-serif','sans-serif':['Helvitica'],'size': 20})
+rc('text', **{'usetex':'True'})
+rc('axes', **{'grid':'True','labelsize':24,'linewidth':1.5,})
+rc('xtick',**{'direction':'in','labelsize':20,'minor.visible':True,'major.size': 10,'major.width': 1.5, \
+              'minor.size': 3.72,'major.width': 0.8})
+rc('ytick',**{'direction':'in','labelsize':20,'minor.visible':True,'major.size': 10,'major.width': 1.5, \
+              'minor.size': 3.72,'major.width': 0.8})
+rc('legend',**{'frameon':False,'numpoints':1,'scatterpoints':1,'fontsize':18})
+rc('figure',**{'autolayout':True})
+rc('grid',**{'alpha':0.6,'linewidth':0.8,'linestyle':'--'})
+
+new_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+              '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+              '#bcbd22', '#17becf']
+new_colors = [mpl.colors.to_rgba(color_i) for color_i in new_colors]
